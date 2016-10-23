82e010aa437cf2fe9113db7886766cc94b241563
Jack Daniel McNamara <Jack Daniel McNamara@DESKTOP-QISAADU>
2016-10-23 20:41:58 +0100

    Jack - Removing VS template classes

--- WebApplication5/App_Start/RouteConfig.cs
+++ WebApplication5/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NetCash
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Transfer", action = "Transfer", id = UrlParameter.Optional }
             );
         }
     }
--- WebApplication5/Global.asax.cs
+++ WebApplication5/Global.asax.cs
@@ -13,7 +13,6 @@ namespace NetCash
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
