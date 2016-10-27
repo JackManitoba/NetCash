c9cf8c2e26fb078538f00771d3dfc76700288799
dermot <dermot@741CY8W1>
2016-10-27 14:30:48 +0100

    Dermot - Basic Insurance Implementation

---
+++ WebApplication5/Controllers/InsuranceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetCash.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        string apply = "thank you";
+
+        // GET: Insurance
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult InsuranceCustomer()
+        {
+            return View(new Models.Insurance());
+        }
+        [HttpPost]
+        public ActionResult InsuranceCustomer(Models.Insurance Insurance)
+        {
+            return View();
+
+
+        }
+    }
+}
---
+++ WebApplication5/Models/Insurance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NetCash.Models
+{
+    public class Insurance { 
+        public string apply = " thank you ";
+
+        public class InsuranceCustomer 
+    {
+        public int InsuranceRisk { get; set; }
+        public string Value { get; set; }
+        }
+
+        [Required]
+        [Display(Name = "Customers age")]
+        public int age { get; set; }
+
+        [Required]
+        [Display(Name = "What is your job risk low to high (1-4)")]
+        public string risk { get; set; }
+       
+
+        public IEnumerable<InsuranceCustomer> InsuranceRisk =
+            new List<InsuranceCustomer>
+            {
+                new InsuranceCustomer {InsuranceRisk = 0, Value = "1" },
+                new InsuranceCustomer {InsuranceRisk = 1, Value = "2" },
+                new InsuranceCustomer {InsuranceRisk = 2, Value = "3" },
+                new InsuranceCustomer {InsuranceRisk = 3, Value = "4" },
+            };
+
+    }
+}
