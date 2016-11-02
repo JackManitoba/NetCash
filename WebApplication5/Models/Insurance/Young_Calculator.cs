a8e09a7556ece7ed98b92442503461af605f5549
dermot <dermot@741CY8W1>
2016-11-02 15:57:49 +0000

    Dermot - Added Insurance calculators (Strategy Pattern)

---
+++ WebApplication5/Models/Insurance/Young_Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models.Insurance
+{
+    public class Young_Calculator : IInsuranceStrategy
+    {
+    
+        public double calculatequte(int province, int insuranceType)
+        {
+            return province * 50   + insuranceType * 25 + 600;
+        }
+    }
+}
