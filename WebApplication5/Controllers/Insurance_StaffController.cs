9f70e08fa17ee83ac8cea2b6d5d95db31f86ea7a
Dermot Ratigan <dermot.rattigan@gmail.com>
2016-11-09 19:39:45 +0000

    Dermot - Added Staff insurance queries

---
+++ WebApplication5/Controllers/Insurance_StaffController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NetCash.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class Insurance_StaffController : Controller
+    {
+        // GET: Insurance_Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Insurance_StaffQuery()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insurance_StaffQuery(Insurance insurance)
+        {
+            insurance.GetInsuranceByAccountNumber();
+            return View("DiscussInsurance", insurance);
+        }
+        public ActionResult DiscussInsurance(Insurance insurance)
+        {
+            insurance.MarkInsuranceAsDiscussed();
+            return View();
+        }
+
+    }
+}
--- WebApplication5/Models/Insurance/Insurance.cs
+++ WebApplication5/Models/Insurance/Insurance.cs
@@ -4,6 +4,10 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using WebApplication5.Models.Insurance;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Diagnostics;
 
 namespace NetCash.Models
 {
@@ -11,13 +15,55 @@ namespace NetCash.Models
         public string apply = " thank you ";
         public double quote { get; set; }
 
-         IInsuranceStrategy strategy { get; set; }
+        public string AccountNumber { get; set; }
 
+        public DateTime DateOfApplication { get; set; }
+
+        public bool Discussed { get; set; }
+
+        IInsuranceStrategy strategy { get; set; }
+
+        public static List<InsuranceType> InsuranceTypeList = new List<InsuranceType>
+            {
+                new InsuranceType {TypeIntValue = 1, StringValue = "Travel" },
+                new InsuranceType {TypeIntValue = 2, StringValue = "Car" },
+                new InsuranceType {TypeIntValue = 3, StringValue = "Personal" },
+                new InsuranceType {TypeIntValue = 4, StringValue = "Home" },
+            };
+
+        public static List<AgeType> AgeTypeList = new List<AgeType>
+           {
+                new AgeType {ageBracketIntValue = 1, StringValue = "0-30" },
+                new AgeType {ageBracketIntValue = 2, StringValue = "30-65" },
+                new AgeType {ageBracketIntValue = 3, StringValue = "65+" },
+           };
 
 
         public void CalculatePremium()
         {
-            quote = strategy.calculatequote(LocationChoice, AgeChoice);
+            quote = strategy.calculatequote(
+                LocationTypeList.Find(locationType => String.Equals(locationType.StringValue, LocationChoice)).LocationIntValue,
+                AgeTypeList.Find(AgeType => String.Equals(AgeType.StringValue, AgeChoice)).ageBracketIntValue);
+        }
+
+        public class InsuranceType
+        {
+
+            public string StringValue { get; set; }
+            public int TypeIntValue { get; set; }
+        }
+
+        public class AgeType
+        {
+
+            public string StringValue { get; set; }
+            public int ageBracketIntValue { get; set; }
+        }
+
+        public class LocationType
+        {
+            public int LocationIntValue { get; set; }
+            public string StringValue { get; set; }
         }
 
         public class InsuranceCustomer 
@@ -33,16 +79,16 @@ namespace NetCash.Models
         {
             switch(InsuranceTypeChoice)
             {
-                case 1:
+                case "Travel":
                     strategy = new Travel_Insurance();
                     break;
-                case 2:
+                case "Car":
                     strategy = new Car_Insurance();
                     break;
-                case 3:
+                case "Personal":
                     strategy = new Persnal_Insurance();
                     break;
-                case 4:
+                case "Home":
                     strategy = new Home_insurance();
                     break;
             }
@@ -50,41 +96,109 @@ namespace NetCash.Models
 
         [Required]
         [Display(Name = "What type of insurance are you looking for?")]
-        public int InsuranceTypeChoice { get; set; }
-       
+        public string InsuranceTypeChoice { get; set; }
 
-        public IEnumerable<InsuranceCustomer> InsuranceTypeOptions =
-            new List<InsuranceCustomer>
-            {
-                new InsuranceCustomer {InsuranceType = 1, Value = "Travel" },
-                new InsuranceCustomer {InsuranceType = 2, Value = "Car" },
-                new InsuranceCustomer {InsuranceType = 3, Value = "Personal" },
-                new InsuranceCustomer {InsuranceType = 4, Value = "Home" },
-            };
 
+        public IEnumerable<InsuranceType> InsuranceTypeOptions = InsuranceTypeList;
         [Required]
         [Display(Name = "What is your current age bracket?")]
-        public int AgeChoice { get; set; }
+        public string AgeChoice { get; set; }
 
-        public IEnumerable<InsuranceCustomer> ageBracketOptions =
-           new List<InsuranceCustomer>
+        public IEnumerable<AgeType> ageBracketOptions = AgeTypeList;
+           
+
+        public static List<LocationType> LocationTypeList = new List<LocationType>
            {
-                new InsuranceCustomer {ageBracket = 1, Value = "0-30" },
-                new InsuranceCustomer {ageBracket = 2, Value = "30-65" },
-                new InsuranceCustomer {ageBracket = 3, Value = "65+" },
+                new LocationType {LocationIntValue = 1, StringValue = "Connacht" },
+                new LocationType {LocationIntValue = 2, StringValue = "Munster" },
+                new LocationType {LocationIntValue = 3, StringValue = "Leinster" },
+                new LocationType {LocationIntValue = 4, StringValue = "Ulster" }
            };
 
         [Required]
         [Display(Name = "In which province do you currently reside?")]
-        public int LocationChoice { get; set; }
+        public string LocationChoice { get; set; }
 
-        public IEnumerable<InsuranceCustomer> LocationOptions =
-           new List<InsuranceCustomer>
-           {
-                new InsuranceCustomer {Location = 1, Value = "Connacht" },
-                new InsuranceCustomer {Location = 2, Value = "Munster" },
-                new InsuranceCustomer {Location = 3, Value = "Leinster" },
-                new InsuranceCustomer {Location = 4, Value = "Ulster" }
-           };
+        public IEnumerable<LocationType> LocationOptions = LocationTypeList;
+
+
+
+    public void SubmitApplication(string AccountNumber)
+    {
+        using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+        {
+
+            string _sql = @"INSERT INTO [dbo].[InsuranceQuery] (GUID, AccountNumber, Insurancetype, AgeBracket, Location, Date, Discussed) VALUES (@id, @an,@in, @ab, @lc, @dt, @d)";
+
+            var cmd = new SqlCommand(_sql, connection);
+
+            cmd.Parameters
+                .Add(new SqlParameter("@id", SqlDbType.UniqueIdentifier))
+                .Value = Guid.NewGuid();
+            cmd.Parameters
+                .Add(new SqlParameter("@an", SqlDbType.NVarChar))
+                .Value = AccountNumber.ToString();
+            cmd.Parameters
+                .Add(new SqlParameter("@in", SqlDbType.NVarChar))
+                .Value = InsuranceTypeChoice;
+            cmd.Parameters
+                .Add(new SqlParameter("@ab", SqlDbType.NVarChar))
+                .Value = AgeChoice;
+            cmd.Parameters
+                .Add(new SqlParameter("@lc", SqlDbType.NVarChar))
+                .Value = LocationChoice;
+            cmd.Parameters
+               .Add(new SqlParameter("@dt", SqlDbType.DateTime))
+               .Value = DateTime.Now;
+            cmd.Parameters
+               .Add(new SqlParameter("@d", SqlDbType.Bit))
+               .Value = 0;
+
+            connection.Open();
+
+            cmd.ExecuteScalar();
+
+            cmd.Dispose();
+            connection.Dispose();
+        }
     }
+
+    public void GetInsuranceByAccountNumber()
+    {
+        using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+        {
+            string _sql = @"SELECT * FROM [dbo].[InsuranceQuery] " +
+                              @"WHERE [AccountNumber] = @an" ;
+
+            Debug.WriteLine(AccountNumber);
+            var cmd = new SqlCommand(_sql, connection);
+            cmd.Parameters
+                .Add(new SqlParameter("@an", SqlDbType.NVarChar))
+                .Value = this.AccountNumber;
+            connection.Open();
+
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                Debug.WriteLine("well");
+                this.InsuranceTypeChoice = reader.GetString(reader.GetOrdinal("Insurancetype"));
+                this.AgeChoice = reader.GetString(reader.GetOrdinal("AgeBracket"));
+                this.LocationChoice = reader.GetString(reader.GetOrdinal("Location"));
+                this.DateOfApplication = reader.GetDateTime(reader.GetOrdinal("Date"));
+                reader.Dispose();
+                cmd.Dispose();
+            }
+            else
+            {
+                reader.Dispose();
+                cmd.Dispose();
+            }
+        }
+    }
+
+    public void MarkInsuranceAsDiscussed()
+    {
+        Debug.WriteLine("well done");
+    }
+        }
 }
