0f39c30590066cb161d6946ee1e069dfd76d18a0
Mark <mrkhynes1@gmail.com>
2016-11-14 18:37:30 +0000

    Mark - ATM project can now run as it's own application

---
+++ NetCashATM/NetCashATM/ATMDriver.cs
@@ -0,0 +1,77 @@
+﻿
+using NetCashATM.UserInterface.Panels;
+using NetCashATM.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using NetCashATM.Observers;
+using BankingFramework.InterceptorPackage.ContextObjects;
+using BankingFramework.InterceptorPackage.Interceptors;
+using BankingFramework.InterceptorPackage.Dispatchers;
+
+namespace ClientCode
+{
+
+    class Program
+    {
+
+        private static ATMMainView _mainView;
+
+        public int VerifiedSession = 0;
+
+        class NavigationRequestInterceptor : Interceptor
+        {
+            PanelFactory _panelFactory = new PanelFactory();
+
+            public void consumeService(ContextObject e)
+            {
+
+                Debug.WriteLine("Program.NavigationRequestInterceptor.BaseFunction: " + e.GetShortDescription());
+                _mainView.SetCurrentPanel(_panelFactory.GetPanel(e.GetShortDescription()));
+            }
+        }
+
+        public List<Subject> SubjectList
+        {
+            get
+            {
+                return SubjectList;
+            }
+        }
+
+        public static void SetPanel(ATMPanel currentPanel)
+        {
+            _mainView.SetCurrentPanel(currentPanel);
+        }
+
+        public static void SetDataPath()
+        {
+            var path = (AppDomain.CurrentDomain.BaseDirectory);
+            int position = path.IndexOf("NetCash");
+            var substring = path.Substring(0,position);
+            path = substring + "NetCash\\NetCashWebSite\\App_Data";
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
+
+
+        [STAThread]
+        public static void Main()
+        {
+            SetDataPath();
+
+            ClientRequestInterceptor myInterceptor = new ClientRequestInterceptor();
+            ClientRequestDispatcher.TheInstance().RegisterClientInterceptor(myInterceptor);
+            NavigationRequestInterceptor navInterceptor = new NavigationRequestInterceptor();
+            NavigationRequestDispatcher.TheInstance().RegisterClientInterceptor(navInterceptor);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            _mainView = new ATMMainView();
+            PinPanel pinPanel = new PinPanel();
+            SetPanel(pinPanel);
+            Application.Run(_mainView);
+        }
+    }
+}
